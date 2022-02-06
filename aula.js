f5369a97460a9f0b697fd77c76e0af54eb1ad7d9
Edilson Gafanhoto <68605911+edskate@users.noreply.github.com>
2022-02-06 18:18:41 -0300

    Aula de javascript
    atualização.ok

---
+++ aula.js
@@ -0,0 +1,17 @@
+let nome = 'ed';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+let corSelecionado = null;
+
+console.log('lu')
+console.log('idade')
+
+let pessoa = {
+nome: 'lu',
+idade:30,
+estaAprovado: true,
+sobrenome : 'billy'
+};
+
+console.log(pessoa);
